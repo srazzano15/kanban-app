ec9978c97a56c7e2019bdcc25d016d2170be5c6e
srazzano15 <40343937+srazzano15@users.noreply.github.com>
2019-08-23 23:58:58 -0700

    built static page with basic styling

--- server/index.js
+++ server/index.js
@@ -13,6 +13,15 @@ const todos = require('./routes/api/todos')
 
 app.use('/api/todos', todos)
 
+// handle production
+if (process.env.NODE_ENV === 'production') {
+    // static folder
+    app.use(express.static(__dirname + '/public'))
+
+    // handle SPA
+    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'))
+}
+
 const port = process.env.PORT || 5000
 
 app.listen(port, () => console.log(`Server listening on ${port}!`))
